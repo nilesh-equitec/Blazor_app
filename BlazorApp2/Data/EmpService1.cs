40582a35f5540ccb45f3bd5192d16360f1734661
nilesh-equitec <155631504+nilesh-equitec@users.noreply.github.com>
2024-01-15 20:38:20 +0530

    commit

---
+++ BlazorApp2/Data/EmpService1.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using BlazorApp2.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorApp2.Data
+{
+    public class EmpService1
+    {
+        private readonly EmployeeDBContext _dbContext;
+
+        public EmpService1(EmployeeDBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public List<Emp> GetTodoItems()
+        {
+            return _dbContext.Emps.ToList();
+        }
+
+        public void AddTodoItem(Emp todoItem)
+        {
+            _dbContext.Emps.Add(todoItem);
+            _dbContext.SaveChanges();
+        }
+    }
+}
---
+++ BlazorApp2/Data/EmpService2.cs
@@ -0,0 +1,40 @@
+﻿using BlazorApp2.Models;
+
+namespace BlazorApp2.Data
+{
+    public class EmpService2
+    {
+        private readonly EmployeeDBContext _dbContext;
+
+
+        public EmpService2(EmployeeDBContext _dbContext)
+        {
+            this._dbContext = _dbContext;
+        }
+
+        public async Task<List<AllEmployeeResult>> GetAllEmployeesAsync(int? value)
+        {
+            return await _dbContext.Procedures.AllEmployeeAsync(value);
+        }
+
+        public async Task<int> DeleteEmployeeAsync(int? Id, int? value1)
+        {
+            return await _dbContext.Procedures.DelectEmployee1Async(Id, value1);
+        }
+
+        public async Task<int> InsertEmployeeAsync(string Name, string Email, string Phone, string Gender, string Skill, string Address)
+        {
+            return await _dbContext.Procedures.InsertEmployeeAsync(Name, Email, Phone, Gender, Skill, Address);
+        }
+
+        public async Task<List<SelectById1Result>> GetEmployeeByIdAsync(int? value)
+        {
+            return await _dbContext.Procedures.SelectById1Async(value);
+        }
+
+        public async Task<int> UpdateEmployeeAsync(int? Id, string Name, string Email, string Phone, string Address, string Skill)
+        {
+            return await _dbContext.Procedures.UpdateEmployeeAsync(Id, Name, Email, Phone, Address, Skill);
+        }
+    }
+}
